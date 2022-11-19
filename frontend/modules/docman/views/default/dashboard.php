1007d56adb0956dfbfe7cfd441c6970e6c12f80a
arismoratalla <arismoratalla@gmail.com>
2022-11-20 01:31:30 +0800

    Update dashboard.php

--- frontend/modules/docman/views/default/dashboard.php
+++ frontend/modules/docman/views/default/dashboard.php
@@ -121,37 +121,7 @@ Modal::end();
             <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
           </div>
         </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
-          <div class="small-box bg-teal">
-            <div class="inner">
-              <h3><?= 100 ?></h3>
-
-              <p>FOR APPROVAL</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-pie-graph"></i>
-            </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
-          <div class="small-box bg-maroon">
-            <div class="inner">
-              <h3><?= 100 ?></h3>
-
-              <p>FOR PAYMENT</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-pie-graph"></i>
-            </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-        <!-- ./col -->
+        
       </div>
 
     </section>
