5698b9244b45d48af3dcdc74b9eefe5a399cf155
arismoratalla <arismoratalla@gmail.com>
2023-01-31 16:45:02 +0800

    updating images

--- frontend/modules/docman/views/default/index.php
+++ frontend/modules/docman/views/default/index.php
@@ -5,7 +5,7 @@ $this->params['breadcrumbs'] = "";
 ?>
 <div class="d-flex align-items-center">
   <div class="flex-shrink-0 align-items-center">
-    <img src="/images/dms-portal.png" style="width:40%; margin: auto; display: block;">
+    <img src="/images/dms-reminder.png" style="width:40%; margin: auto; display: block;">
   </div>
 </div>
 
