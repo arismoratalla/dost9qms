bfedd59fec39e71411bad62a16f0abf2ca3d6d5b
arismoratalla <arismoratalla@gmail.com>
2023-03-23 00:44:05 +0800

    Update dashboard.php

--- frontend/modules/riskman/views/default/dashboard.php
+++ frontend/modules/riskman/views/default/dashboard.php
@@ -23,7 +23,7 @@ Modal::end();
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-        Dashboard
+        Registry Dashboard
         <small>Control panel</small>
       </h1>
     </section>
