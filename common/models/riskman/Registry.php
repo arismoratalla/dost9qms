05389723b49d98bc68cc34d40ad77edbb3986a88
arismoratalla <arismoratalla@gmail.com>
2023-04-11 06:57:27 +0800

    Update Registry.php

--- common/models/riskman/Registry.php
+++ common/models/riskman/Registry.php
@@ -44,7 +44,7 @@ class Registry extends \yii\db\ActiveRecord
     {
         return [
             [['registry_type', 'customer_requirement', 'potential'], 'string'],
-            [['source_id', 'unit_id', 'created_by', 'area_id','stakeholders', 'potential'], 'required'],
+            [['source_id', 'unit_id', 'area_id','stakeholders', 'potential'], 'required'],
             [['unit_id', 'area_id','created_by',  'active'], 'integer'],
             [['code','previous_evaluation', 'assessment_id', 'evaluation_id','create_date'], 'safe'],
             [['code'], 'string', 'max' => 50],
