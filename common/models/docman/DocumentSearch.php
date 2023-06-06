9a5dd5aef4ae5ac8e9e0033f3b9e12d648608f41
arismoratalla <arismoratalla@gmail.com>
2023-06-06 14:10:02 +0800

    Updated DocumentSearch

--- common/models/docman/DocumentSearch.php
+++ common/models/docman/DocumentSearch.php
@@ -76,7 +76,7 @@ class DocumentSearch extends Document
         if( !(Yii::$app->user->can('17025-auditor') || Yii::$app->user->can('17025-document-custodian') || (Yii::$app->user->identity->username == 'Admin') ) ){
             if(isset($_GET['category_id'])){
                 $query->andFilterWhere(['in', 'category_id', 
-                    [1,2,3,5,6,7,8,9,10,12,13,14,15,16,17,18,19,20,21,22,23,24]]
+                    [1,2,3,5,6,7,8,9,10,12,13,14,15,16,17,18,19,20,21,22,23,24,25]]
                 );
             }
             if(isset($_GET['functional_unit_id'])){
